ffed6b3c86c00522b75eccbd8200fe2989d63f7e
mdsharia4613 <mdsharia4613@gmail.com>
2025-11-18 10:17:23 +0600

    card workoing

--- src/app/products/TopRating.jsx
+++ src/app/products/TopRating.jsx
@@ -2,6 +2,8 @@
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 import { HiOutlineShoppingBag } from "react-icons/hi";
+import { FaRegStar, FaStar, FaStarHalf } from "react-icons/fa6";
+
 
 const TopRating = () => {
     const [products, setProducts] = useState([]);
@@ -20,41 +22,54 @@ const TopRating = () => {
         <div className='my-10'>
             <h2 className='text-3xl text-center font-semibold'>Popular product</h2>
             <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 container mx-auto p-20">
-                {products.map(item => (
-                    <div
-                        key={item.id}
-                        className=" shadow-lg rounded-lg p-3 hover:shadow-xl transition duration-300"
-                    >
-                        {/* Product Image */}
-                        <div className="w-full h-48 overflow-hidden rounded-lg">
-                            <Image
-                                src={item.image}
-                                alt={item.title}
-                                width={400}
-                                height={300}
-                                className="object-cover w-full h-full transition-transform duration-500 hover:rotate-x-180"
-                            />
-                        </div>
+                {products.map(item => {
+                    const stars = item.rating;
+                    const fullStars = Math.floor(stars);
+                    const halStar = stars % 1 >= 0.5;
+                    return (
+                        <div
+                            key={item.id}
+                            className=" shadow-lg rounded-lg p-3 hover:shadow-xl transition duration-300 relative"
+                        >
+                            {/* Product Image */}
+                            <div className="w-full h-48 overflow-hidden rounded-lg">
+                                <Image
+                                    src={item.image}
+                                    alt={item.title}
+                                    width={400}
+                                    height={300}
+                                    className="object-cover w-full h-full transition-transform duration-500 hover:rotate-x-180"
+                                />
+                            </div>
 
 
-                        {/* Title */}
-                        <h2 className="text-lg font-semibold mt-3">{item.title}</h2>
-                        <p>{item.id}</p>
+                            {/* Title */}
+                            <h2 className="text-lg font-semibold mt-3">{item.title}</h2>
+                            <p>{item.id}</p>
 
-                        {/* Rating */}
-                        <p className="mt-1 text-blue-500 font-medium">
-                            ⭐ {item.rating}
-                        </p>
-
-                        {/* Price + Cart */}
-                        <div className="flex justify-between items-center mt-3">
-                            <p className="text-lg font-bold text-blue-600">{item.price} ৳</p>
-                            <button className="bg-blue-600 text-white p-2 rounded-lg hover:bg-blue-700 transition cursor-pointer">
+                            {/* Rating */}
+                            <div className="flex items-center gap-2">
+                                <div className="flex">
+                                    {Array.from({ length: 5 }, (_, i) => {
+                                        if (i < fullStars) return <FaStar key={i} className="text-yellow-400" />;
+                                        else if (i === fullStars && halStar) return <FaStarHalf key={i} className="text-yellow-400" />;
+                                        else return <FaRegStar key={i} className="text-yellow-400" />;
+                                    })}
+                                </div>
+                                <span>{item.rating}</span>
+                            </div>
+                            <button className="bg-blue-600 text-white p-2 rounded-lg hover:bg-blue-700 transition cursor-pointer absolute right-1 bottom-20">
                                 <HiOutlineShoppingBag size={22} />
                             </button>
+
+                            {/* Price + Cart */}
+                            <div className="flex justify-between items-center mt-3">
+                                <p className="text-lg font-bold text-blue-600">{item.price} ৳</p>
+                                
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    )
+                })}
             </div>
         </div>
     );
